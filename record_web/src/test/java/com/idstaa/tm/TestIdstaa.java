3ec88afbaee8f7f9ec9c68c757d55cf1af923ee4
chenjie <chenjie@erwan365.com>
2021-08-07 15:09:05 +0800

    配置修改

---
+++ record_web/src/test/java/com/idstaa/tm/TestIdstaa.java
@@ -0,0 +1,17 @@
+package com.idstaa.tm;
+
+/**
+ * @author chenjie
+ * @date 2021/6/7 16:21
+ */
+public class TestIdstaa {
+    private String idstaa;
+
+    public String getIdstaa() {
+        return idstaa;
+    }
+
+    public void setIdstaa(String idstaa) {
+        this.idstaa = idstaa;
+    }
+}
